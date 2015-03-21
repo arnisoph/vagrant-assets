887a612b208b44bfb139d57443fbf8fe97235596
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-03-21 15:53:58 +0100

    improve saltstack_services provision script

--- scripts/provision/debian/custom/saltstack_services/A_base.sh
+++ scripts/provision/debian/custom/saltstack_services/A_base.sh
@@ -4,6 +4,8 @@ echo '##############################################'
 echo "Starting ${0}.."
 set -x
 
+echo "Restarting all Salt services.."
+
 [[ -n "$(which salt-master)" ]] && service salt-master restart
 [[ -n "$(which salt-syndic)" ]] && service salt-syndic restart
 [[ -n "$(which salt-minion)" ]] && service salt-minion restart
