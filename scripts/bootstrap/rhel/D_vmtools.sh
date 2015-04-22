1cd4a75166b74963a82ef2b27f30f3fc17d0ee24
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-04-22 16:59:39 +0200

    simply do yum -y install dkms

--- scripts/bootstrap/rhel/D_vmtools.sh
+++ scripts/bootstrap/rhel/D_vmtools.sh
@@ -14,17 +14,19 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     ln -s /usr/include/drm/drm_fourcc.h drm_fourcc.h
   fi
 
-  # optional: add Centos 6 dkms support, always downloads latest available
-  if [[ $(grep ' 6.' /etc/redhat-release) ]]; then
-    wget -P /tmp -nd -nc -A rpm -l1 -r http://ftp.hosteurope.de/mirror/fedora-epel/6/x86_64/ --accept-regex 'dkms.+rpm$'
-    sudo rpm -ivh /tmp/dkms*
-  fi
+  ## optional: add Centos 6 dkms support, always downloads latest available
+  #if [[ $(grep ' 6.' /etc/redhat-release) ]]; then
+  #  #wget -P /tmp -nd -nc -A rpm -l1 -r http://ftp.hosteurope.de/mirror/fedora-epel/6/x86_64/ --accept-regex 'dkms.+rpm$'
+  #  sudo rpm -ivh /tmp/dkms*
+  #fi
 
-  # optional: add Centos 7 dkms support, always downloads latest available
-  if [[ $(grep ' 7.' /etc/redhat-release) ]]; then
-    wget -P /tmp/  -nd -nc -A rpm -l1 -r http://ftp.hosteurope.de/mirror/fedora-epel/7/x86_64/d/ --accept-regex 'dkms.+rpm$'
-    sudo rpm -ivh /tmp/dkms*
-  fi
+  ## optional: add Centos 7 dkms support, always downloads latest available
+  #if [[ $(grep ' 7.' /etc/redhat-release) ]]; then
+  #  wget -P /tmp/  -nd -nc -A rpm -l1 -r http://ftp.hosteurope.de/mirror/fedora-epel/7/x86_64/d/ --accept-regex 'dkms.+rpm$'
+  #  sudo rpm -ivh /tmp/dkms*
+  #fi
+  # the following requires the EPEL repo
+  yum -y install dkms
 
   # install VirtualBox guest additions
   VBOX_VERSION=$(cat /etc/vbox_version)
@@ -47,7 +49,7 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
   rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub
   rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
 
-  # add repository
+  # add repository # TODO is this still required on el7?
   echo '[vmware-tools]' > /etc/yum.repos.d/vmware-tools.repo
   echo 'name=VMware Tools' >> /etc/yum.repos.d/vmware-tools.repo
   echo 'baseurl=http://packages.vmware.com/tools/esx/5.5p01/rhel6/x86_64/' >> /etc/yum.repos.d/vmware-tools.repo
@@ -55,7 +57,7 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
   echo 'gpgcheck=1' >> /etc/yum.repos.d/vmware-tools.repo
 
   # install vmware tools
-  yum install -y vmware-tools-esx-nox
+  yum -y install vmware-tools-esx-nox
 fi
 
 echo "Finishing ${0}.."
