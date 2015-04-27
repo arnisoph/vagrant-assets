63f25909d0de4aed408ffd77660f6ff7f2520a2d
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-04-27 19:00:18 +0200

    set timezone on el7

--- scripts/provision/rhel/custom/basic/A_base.sh
+++ scripts/provision/rhel/custom/basic/A_base.sh
@@ -4,4 +4,9 @@ echo '##############################################'
 echo "Starting ${0}.."
 set -x
 
+if [[ $(grep -E '^[A-Za-z ]*7\.' /etc/redhat-release) ]]; then
+  #TODO make this configurable:
+  timedatectl set-timezone Europe/Berlin
+fi
+
 echo "Finishing ${0}.."
