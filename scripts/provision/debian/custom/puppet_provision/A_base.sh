9fd04a8cfb12e11a944a796ebbb65a0d4244948a
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-04-28 13:27:43 +0200

    lookup --test

--- scripts/provision/debian/custom/puppet_provision/A_base.sh
+++ scripts/provision/debian/custom/puppet_provision/A_base.sh
@@ -5,7 +5,10 @@ echo "Starting ${0}.."
 set -x
 
 puppet apply \
-  --test \
+  --ignorecache \
+  --verbose \
+  --show_diff \
+  --detailed-exitcodes \
   --modulepath=/vagrant/share/puppet/modules_custom/:/vagrant/share/puppet/modules/:/vagrant/share/puppet/modules_dist/ \
   --hiera_config=/vagrant/share/puppet/hiera.yaml \
   /vagrant/share/puppet/manifests/$(hostname -s)/default.pp
