6139086d3273866d35648b85f254e1217f0fb21d
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-04-28 15:50:49 +0200

    also disable firewall on provisioning

--- scripts/provision/rhel/custom/basic/A_base.sh
+++ scripts/provision/rhel/custom/basic/A_base.sh
@@ -8,6 +8,7 @@ if [[ $(grep -E '^[A-Za-z ]*7\.' /etc/redhat-release) ]]; then
   #TODO make this configurable:
   timedatectl set-timezone Europe/Berlin
   systemctl stop firewalld
+  systemctl disable firewalld
 fi
 
 echo "Finishing ${0}.."
