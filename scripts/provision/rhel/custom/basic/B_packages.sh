053fa0b6fdc995f5396442f632591e33623b1c07
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-06-20 23:43:11 +0200

    improve proxy usage on EL

--- scripts/provision/rhel/custom/basic/B_packages.sh
+++ scripts/provision/rhel/custom/basic/B_packages.sh
@@ -7,14 +7,18 @@ set -x
 update_dist=$ENV_basic_update_dist
 pkg_proxy_uri=$ENV_basic_pkg_proxy_uri
 
+yum -y install \
+  epel-release \
+  ed
+
 if [[ -n "$pkg_proxy_uri" ]]; then
   echo "proxy=${pkg_proxy_uri}" >> /etc/yum.conf
+  find /etc/yum.repos.d/ -type f -name '*.repo' -exec ed -s {} <<< $',s/#baseurl=/baseurl=/g\nw' \;
+  find /etc/yum.repos.d/ -type f -name '*.repo' -exec ed -s {} <<< $',s/mirrorlist=/#mirrorlist=/g\nw' \;
+  [[ -e /etc/yum/pluginconf.d/fastestmirror.conf ]] && ed -s /etc/yum/pluginconf.d/fastestmirror.conf <<< $',s/enabled=1/enabled=0/g\nw' \;
 fi
 
 [[ "$update_dist" == 'true' ]] && yum -y update
-
-yum -y install \
-  epel-release \
 
 yum -y install \
   perl \
