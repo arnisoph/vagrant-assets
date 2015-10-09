7781a86996e0562a4ff698b55a8b56dc3fd1a5c3
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-10-09 23:19:34 +0200

    remove OS_CODENAME again, we need lsb_release anyway..

--- scripts/bootstrap/debian/2_packages.sh
+++ scripts/bootstrap/debian/2_packages.sh
@@ -4,24 +4,27 @@ echo '##############################################'
 echo "Starting ${0}.."
 set -x
 
+apt-get -q update
+DEBIAN_FRONTEND=noninteractive apt-get -yV install lsb-release
+
 if [[ -n "$MANAGE_SOURCES_LIST" ]]; then #TODO make it more configurable
 cat <<EOF > /etc/apt/sources.list
-deb http://ftp.debian.org/debian ${OS_CODENAME} main contrib non-free
-deb-src http://ftp.debian.org/debian ${OS_CODENAME} main contrib non-free
+deb http://ftp.debian.org/debian $(lsb_release -cs) main contrib non-free
+deb-src http://ftp.debian.org/debian $(lsb_release -cs) main contrib non-free
 
-deb http://security.debian.org/ ${OS_CODENAME}/updates main contrib non-free
-deb-src http://security.debian.org/ ${OS_CODENAME}/updates main contrib non-free
+deb http://security.debian.org/ $(lsb_release -cs)/updates main contrib non-free
+deb-src http://security.debian.org/ $(lsb_release -cs)/updates main contrib non-free
 
-deb http://ftp.debian.org/debian ${OS_CODENAME}-updates main contrib non-free
-deb-src http://ftp.debian.org/debian ${OS_CODENAME}-updates main contrib non-free
+deb http://ftp.debian.org/debian $(lsb_release -cs)-updates main contrib non-free
+deb-src http://ftp.debian.org/debian $(lsb_release -cs)-updates main contrib non-free
 
-deb http://ftp.debian.org/debian ${OS_CODENAME}-backports main contrib non-free
-deb-src http://ftp.debian.org/debian ${OS_CODENAME}-backports main contrib non-free
+deb http://ftp.debian.org/debian $(lsb_release -cs)-backports main contrib non-free
+deb-src http://ftp.debian.org/debian $(lsb_release -cs)-backports main contrib non-free
 EOF
 fi
 
 apt-get -q update
 DEBIAN_FRONTEND=noninteractive apt-get -yV upgrade
-DEBIAN_FRONTEND=noninteractive apt-get -yV install apt-transport-https bind9-host wget apt-utils curl net-tools lsb-release
+DEBIAN_FRONTEND=noninteractive apt-get -yV install apt-transport-https bind9-host wget apt-utils curl net-tools
 
 echo "Finishing ${0}.."
