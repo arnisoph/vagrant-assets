bc47c5844d12f87aca8d93275b1de6a26ec52b95
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2016-11-03 00:33:35 +0100

    deploy extra ssh keys

--- scripts/provision/debian/custom/basic/1_base.sh
+++ scripts/provision/debian/custom/basic/1_base.sh
@@ -8,4 +8,13 @@ if [[ -d /vagrant/shared/misc/tools/ && -d /etc/profile.d/ ]]; then
   echo 'export PATH=${PATH}:/vagrant/shared/misc/tools/' > /etc/profile.d/vagrant_basic.sh
 fi
 
+ssh_pubkeys=$ENV_basic_ssh_pubkeys
+if [[ -n "${ssh_pubkeys}" ]]; then
+  mkdir -p /root/.ssh/
+  echo -e "${ssh_pubkeys}" > /root/.ssh/authorized_keys
+  chmod 700 /root/.ssh/
+  chmod 640 /root/.ssh/authorized_keys
+  chown -R root:root /root/.ssh/
+fi
+
 echo "Finishing ${0}.."
